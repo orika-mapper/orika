b3190feb5b9f27905cace7a9b1bbe322b5fc7273
ikozar <ikozar@gmail.com>
2013-03-21 12:14:59 +0400

    no

--- configurator/src/main/java/ma/glasnost/orika/loader/DefaultParser.java
+++ configurator/src/main/java/ma/glasnost/orika/loader/DefaultParser.java
@@ -7,28 +7,29 @@ import org.slf4j.LoggerFactory;
 
 import javax.xml.stream.events.Attribute;
 import javax.xml.stream.events.XMLEvent;
+import java.util.EnumMap;
 import java.util.Iterator;
 
 /**
  * @author  ikozar
  * date     13.02.13
  */
-public abstract class DefaultParser<E extends Enum> implements IParser {
+public abstract class DefaultParser<E extends Enum<E>> implements IParser {
     protected static Logger log;
 
     protected IParser parent;           // Loader for parent level
     protected Class<E> enumNodeNames;   // enum contains levels node names
-    protected Enum lastParsedNode;
+    protected E lastParsedNode;
     protected String lastParsedChars;
     protected XMLEvent lastEvent;
     protected Iterator lastAttIterator; // attribute of last node
-    protected EnumMap<String> enumMap;
+    protected EnumMap<E, String> enumMap;
     protected MapperFactory factory;
 
     public DefaultParser(Class<E> clazz, MapperFactory factory) {
         log = LoggerFactory.getLogger(this.getClass());
         enumNodeNames = clazz;
-        enumMap = new EnumMap<String>(enumNodeNames);
+        enumMap = new EnumMap<E, String>(enumNodeNames);
         this.factory = factory;
     }
 
@@ -37,13 +38,13 @@ public abstract class DefaultParser<E extends Enum> implements IParser {
      * @param localPart
      * @return
      */
-    protected Enum findLocalPart(String localPart)
+    protected E findLocalPart(String localPart)
     {
         for (Enum enumVal : enumNodeNames.getEnumConstants())
         {
             if (((IElement) enumVal).getLocalPart().equals(localPart))
             {
-                return enumVal;
+                return (E) enumVal;
             }
         }
         log.error("Undefined element - " + localPart);
